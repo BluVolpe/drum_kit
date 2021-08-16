aa9a5f4826a3d45c980fe25500c1275bfc4bc14d
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-08-16 00:06:56 -0400

    added eventListeners for audio + keyDown & added function to remove CSS transtion after keyDown

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 window.addEventListener('keydown', function(event) {    
+    // console.log(event);
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    // console.log(audio);
+    if(!audio) return; // stop the function from running
+    audio.currentTime = 0; // rewinds audio in order to repeat sounds
+    audio.play();
+    // console.log(key);
+    key.classList.add("playing"); // adding a class to change the CSS properties
+});
+
+function removeTransition(event) {
     console.log(event);
-})
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
