a74fe6ac74e850a3c0cabfc521e5184a3e052ea4
Blu <blu.volpe@gmail.com>
2021-08-25 16:11:52 -0400

    Added sound/class to on click for clap + fixed font for phone

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ function playSound(event) {
   key.classList.add("playing"); // adding a class to change the CSS properties
 }
 
+document.getElementById("a").addEventListener("click", function() {
+  let a = new Audio("sounds/clap.wav");
+  this.classList.add("playing");
+  a.play()
+  
+});
+
 function removeTransition(event) {
   // console.log(event);
   if (event.propertyName !== "transform") {
